d0db9f08f666a7dcf2f3d1654efa9e0d5295f9b0
Akanksha Saran <akankshasaran@gmail.com>
2019-04-18 18:05:36 -0500

    show action labels

--- scripts/visualize_gaze_atari.py
+++ scripts/visualize_gaze_atari.py
@@ -1,15 +1,48 @@
 import numpy as np
 import cv2
 
-img_folder = '100_RZ_3592991_Aug-24-11-44-38/'
-file = open("100_RZ_3592991_Aug-24-11-44-38.txt")
+img_folder = '/home/akanksha/Documents/gaze_atari/hero/195_RZ_205678_Jun-28-12-09-00/'
+file = open("/home/akanksha/Documents/gaze_atari/hero/195_RZ_205678_Jun-28-12-09-00.txt")
 fps = 20
 format="XVID"
-outvid='image_video.avi'
+outvid='/home/akanksha/Documents/gaze_atari/hero/195_RZ_205678_Jun-28-12-09-00.avi'
 fourcc = cv2.VideoWriter_fourcc(*format)
 vid = None
 size = None
 is_color = True
+
+
+action_name = { 
+'0': 'PLAYER_A_NOOP',
+
+'1': 'PLAYER_A_FIRE',          
+'2': 'PLAYER_A_UP',             
+'3': 'PLAYER_A_RIGHT',          
+'4': 'PLAYER_A_LEFT',           
+'5': 'PLAYER_A_DOWN',          
+
+'6': 'PLAYER_A_UPRIGHT',        
+'7': 'PLAYER_A_UPLEFT',         
+'8': 'PLAYER_A_DOWNRIGHT',     
+'9': 'PLAYER_A_DOWNLEFT',       
+
+'10': 'PLAYER_A_UPFIRE',        
+'11': 'PLAYER_A_RIGHTFIRE',     
+'12': 'PLAYER_A_LEFTFIRE',      
+'13': 'PLAYER_A_DOWNFIRE',     
+
+'14': 'PLAYER_A_UPRIGHTFIRE',   
+'15': 'PLAYER_A_UPLEFTFIRE',    
+'16': 'PLAYER_A_DOWNRIGHTFIRE', 
+'17': 'PLAYER_A_DOWNLEFTFIRE',
+'null': 'NULL'
+}
+
+font                   = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (10,10)
+fontScale              = 0.25
+fontColor              = (255,255,255)
+lineType               = 1
 
 line = file.readline()
 # print(line)
@@ -44,11 +77,22 @@ for line in file:
 			x = float(gaze[j])
 			y = float(gaze[j+1])
 		cv2.circle(img, (int(x),int(y)), 5, (0,255,0), thickness=1, lineType=8, shift=0)
+
+		# TODO: show action and return on the video
+		print(action_name[action])
+		cv2.putText(img,action_name[action], 
+		    bottomLeftCornerOfText, 
+		    font, 
+		    fontScale,
+		    fontColor,
+		    lineType)
+
+
 	vid.write(img)
 vid.release()
 # return vid
 
 
-# TODO: show action and return on the video
+
 # TODO: repeat frames if the duration of gaze on a frame is >1/20 seconds. Repeat it n/20 times.
 # TODO: remove blinking artefacts
